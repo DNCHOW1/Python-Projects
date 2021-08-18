3d4c3ed18268c35fdf1bdf895c48d79c09442a0b
dien.chau <70815649+DNCHOW1@users.noreply.github.com>
2021-08-17 20:16:01 -0500

    Add files via upload

---
+++ x2volScraping.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import requests, re, os
+import json
+
+login_url = 'https://www.x2vol.com/login.html'
+opp_url = 'https://www.x2vol.com/Opportunities/1511380/FindOpportunities'
